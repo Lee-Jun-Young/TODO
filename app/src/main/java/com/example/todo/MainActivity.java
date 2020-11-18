f438f935e6063db2ca84c5aa4991477afb8e5925
leejy <leejy8506@naver.com>
2020-11-19 01:41:00 +0900

    Android Jetpack, Room
    Use room to implement add and delete functions

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -1,54 +1,141 @@
 package com.example.todo;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tv_today;
-    private ImageView iv_settins;
-    private Button btn_addTodo;
+    private Button btnPlus;
 
-    private SimpleDateFormat dateFormat = new SimpleDateFormat("MMM d, yyyy", Locale.US);
+    private RecyclerAdapter adapter;
+    private RecyclerView recyclerView;
+    private Paint p = new Paint();
+
+    private TodoDatabase db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnPlus = findViewById(R.id.btnPlus);
 
-        tv_today = findViewById(R.id.tv_today);
-        iv_settins = findViewById(R.id.iv_settins);
-        btn_addTodo = findViewById(R.id.btn_addTodo);
+        recyclerView = findViewById(R.id.rv_view);
 
-        Date date = new Date();
-        String time = dateFormat.format(date);
-        tv_today.setText(time);
+        initSwipe();
 
-        iv_settins.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), SettingsActivity.class);
-                startActivity(intent);
-            }
+        btnPlus.setOnClickListener(v -> {
+            // title 입력 다이얼로그를 호출한다.
+            // title 입력하여 리사이클러뷰 addItem
+            final EditText edittext = new EditText(this);
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Item 추가");
+            builder.setMessage("제목을 입력해 주세요.");
+            builder.setView(edittext);
+            builder.setPositiveButton("입력",
+                    (dialog, which) -> {
+                        //제목 입력, DB추가
+                        if (!edittext.getText().toString().isEmpty()) {
+                            new Thread(() -> {
+                                TodoList memo = new TodoList(edittext.getText().toString(),null);
+                                db.todoDao().insert(memo);
+                            }).start();
+
+                        }
+
+                    });
+            builder.setNegativeButton("취소",
+                    (dialog, which) -> {
+                        //취소버튼 클릭
+                    });
+            builder.show();
+
         });
 
-        btn_addTodo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), AddtodoActivity.class);
-                startActivity(intent);
-            }
-        });
 
+        db = TodoDatabase.getDatabase(this);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new RecyclerAdapter(db);
+        recyclerView.setAdapter(adapter);
+
+        //UI 갱신 (라이브데이터 Observer 이용, 해당 디비값이 변화가생기면 실행됨)
+        db.todoDao().getAll().observe(this, data -> adapter.setItem(data));
+
+    }
+
+    private void initSwipe() {
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT /* | ItemTouchHelper.RIGHT */) {
+
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                int position = viewHolder.getAdapterPosition();
+
+                if (direction == ItemTouchHelper.LEFT) {
+                    new Thread(() -> db.todoDao().delete(adapter.getItems().get(position))).start();
+
+                }
+            }
+
+            @Override
+            public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+
+                Bitmap icon;
+                if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE ) {
+
+                    View itemView = viewHolder.itemView;
+                    float height = (float) itemView.getBottom() - (float) itemView.getTop();
+                    float width = height / 3;
+
+                    if (dX > 0) {
+
+                    } else {
+                        p.setColor(Color.parseColor("#D32F2F"));
+                        RectF background = new RectF((float) itemView.getRight() + dX, (float) itemView.getTop(), (float) itemView.getRight(), (float) itemView.getBottom());
+                        c.drawRect(background, p);
+                        /*
+                         * icon 추가할 수 있음.
+                         */
+                        //icon = BitmapFactory.decodeResource(getResources(), R.drawable.icon_png); //vector 불가!
+                        // RectF icon_dest = new RectF((float) itemView.getRight() - 2 * width, (float) itemView.getTop() + width, (float) itemView.getRight() - width, (float) itemView.getBottom() - width);
+                        //c.drawBitmap(icon, null, icon_dest, p);
+                    }
+                }
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+        };
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        itemTouchHelper.attachToRecyclerView(recyclerView);
     }
 
 }
